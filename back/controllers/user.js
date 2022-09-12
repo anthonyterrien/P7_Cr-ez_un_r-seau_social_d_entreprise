0a8938fb03fc5fc4dfc0a0e21aa562e5eec9307f
anthony <anthony.terrien13@gmail.com>
2022-09-12 10:54:39 +0200

    -feat: add path for user

---
+++ back/controllers/user.js
@@ -0,0 +1,9 @@
+
+exports.signup = () => {}
+exports.login = () => {}
+exports.getAllUsers = () => {}
+exports.getUser = () => {}
+exports.updateUser = () => {}
+exports.untrashUser = () => {}
+exports.trashUser = () => {}
+exports.deleteUser = () => {}
---
+++ back/path/user.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const rateLimiter = require('../middleware/rateLimiter');
+const checkToken = require('../middleware/jsonWebToken');
+const userCtrl = require('../controllers/user');
+let router = express.Router();
+
+router.put('', rateLimiter, userCtrl.signup);
+router.post('/login', rateLimiter, userCtrl.login);
+router.get('/', checkToken, userCtrl.getAllUsers);
+router.get('/:id', checkToken, userCtrl.getUser);
+router.patch('/:id', checkToken, userCtrl.updateUser);
+router.post('/untrash/:id', checkToken, userCtrl.untrashUser);
+router.delete('/trash/:id', checkToken, userCtrl.trashUser);
+router.delete('/:id', checkToken, userCtrl.deleteUser);
+
+module.exports = router
