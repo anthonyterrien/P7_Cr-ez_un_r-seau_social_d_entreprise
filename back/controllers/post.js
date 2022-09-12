e75708614bce44bc346a8c033ab31c58517e7234
anthony <anthony.terrien13@gmail.com>
2022-09-12 11:17:38 +0200

    -feat: add path for post

---
+++ back/controllers/post.js
@@ -0,0 +1,9 @@
+
+exports.createPost = () => {}
+exports.getAllPosts = () => {}
+exports.getPost = () => {}
+exports.updatePost = () => {}
+exports.likePost = () => {}
+exports.untrashPost = () => {}
+exports.trashPost = () => {}
+exports.deletePost = () => {}
---
+++ back/path/post.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const checkToken = require('../middleware/jsonWebToken');
+const postCtrl = require('../controllers/post');
+let router = express.Router();
+
+router.put('', checkToken, postCtrl.createPost);
+router.get('', checkToken, postCtrl.getAllPosts);
+router.get('/:id', checkToken, postCtrl.getPost);
+router.patch('/:id', checkToken, postCtrl.updatePost);
+router.post('/:id/like', checkToken, postCtrl.likePost);
+router.post('/untrash/:id', checkToken, postCtrl.untrashPost);
+router.delete('/trash/:id', checkToken, postCtrl.trashPost);
+router.delete('/:id', checkToken, postCtrl.deletePost);
+
+module.exports = router
