1ee0b6bbfc8a83b6b7f8cec06bd865680829ae59
anthony <anthony.terrien13@gmail.com>
2022-09-12 14:55:37 +0200

    -feat: creation of the login function

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,5 +1,6 @@
 const DB = require('../config/dbConfig')
 const User = DB.User
+const jwt = require('jsonwebtoken');
 
 exports.signup = async (req, res) => {
     const {lastName, firstName, pseudo, email, password} = req.body
@@ -33,7 +34,47 @@ exports.signup = async (req, res) => {
     }
 }
 
-exports.login = () => {}
+exports.login = async (req, res) => {
+    const {pseudo, password} = req.body
+
+    // Data verification
+    if (!pseudo || !password) {
+        return res.status(400).json({message: 'missing data'})
+    }
+
+    try {
+        // Checking if the pseudo exists
+        let user = await User.findOne({where: {pseudo: pseudo}, raw: true})
+
+        if (user === null) {
+            return res.status(401).json({message: 'This account does not exists'})
+        }
+        // Checking password
+        if (!await User.checkPassword(password, user.password)) {
+            return res.status(401).json({message: 'Wrong password'})
+        }
+
+        // Token generation and sending
+        const token = jwt.sign({
+                id: user.id,
+                lastName: user.lastName,
+                firstName: user.firstName,
+                email: user.email
+            }, process.env.JWT_SECRET,
+            {expiresIn: process.env.JWT_DURING})
+
+        return res.json({access_token: token})
+
+    } catch (err) {
+
+        if (err.name === 'SequelizeDatabaseError') {
+            res.status(500).json({message: 'Database Error'})
+        } else {
+            res.status(500).json({message: 'Login process failed'})
+        }
+    }
+}
+
 exports.getAllUsers = () => {}
 exports.getUser = () => {}
 exports.updateUser = () => {}
