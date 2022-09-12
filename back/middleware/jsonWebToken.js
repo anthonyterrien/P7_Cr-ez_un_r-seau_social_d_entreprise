73f36731371a0efcdcb757b9788cebf981a22fd8
anthony <anthony.terrien13@gmail.com>
2022-09-12 10:22:35 +0200

    -feat: add middleware 'json web token' And 'rate limiter'

---
+++ back/middleware/jsonWebToken.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+
+// Token recovery
+const extractBearer = authorization => {
+
+    if (typeof authorization !== 'string') {
+        return false
+    }
+    // Isolate the token
+    const matches = authorization.match(/(bearer)\s+(\S+)/i)
+
+    return matches && matches[2]
+}
+
+
+// Verification presence of the token
+const checkToken = (req, res, next) => {
+
+    const token = req.headers.authorization && extractBearer(req.headers.authorization)
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthenticated user'})
+    }
+
+    // Check the validity of the token
+    jwt.verify(token, process.env.JWT_SECRET, (err) => {
+        if (err) {
+            return res.status(401).json({message: 'Bad token'})
+        }
+
+        next()
+    })
+}
+module.exports = checkToken
