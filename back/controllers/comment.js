7aa111a8d5bf794ca518efd87f35ddc02e01082d
anthony <anthony.terrien13@gmail.com>
2022-09-19 15:10:08 +0200

    -feat: creation of the createComment function

--- back/controllers/comment.js
+++ back/controllers/comment.js
@@ -1,5 +1,35 @@
+const DB = require("../config/dbConfig");
+const Comment = DB.Comment
+const Post = DB.Post
 
-exports.createComment = () => {}
+exports.createComment = async (req, res) => {
+    const {postId, content} = req.body
+
+    // Data verification
+    if (!postId || !content) {
+        return res.status(400).json({message: 'Missing parameter'})
+    }
+    // Post Recovery
+    let post = await Post.findOne({
+        where: {id: postId},
+    })
+    // Test if result
+    if (post === null) {
+        return res.status(404).json({message: 'This post does not exist !'})
+    }
+    try {
+        // Comment creation
+        await Comment.create({
+            postId: postId,
+            userId: res.locals.id,
+            content: content,
+        })
+        return res.json({message: 'Comment Created'})
+    } catch (err) {
+        return res.status(500).json({message: 'Database Error'})
+    }
+}
+
 exports.getAllComments = () => {}
 exports.getComments = () => {}
 exports.updateComment = () => {}
