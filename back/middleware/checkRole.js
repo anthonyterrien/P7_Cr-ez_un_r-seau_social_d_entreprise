8c68dfe7f1e30555570cc3958ce96c3ac9e2d544
anthony <anthony.terrien13@gmail.com>
2022-09-19 13:35:49 +0200

    -feat: creation of a new temporary 'checkRoleForPost' middleware for posts

--- back/middleware/checkRole.js
+++ back/middleware/checkRole.js
@@ -1,5 +1,8 @@
 const DB = require("../config/dbConfig");
 const User = DB.User
+const Post = DB.Post
+
+// TODO remove duplicate code
 
 const checkRole = async (req, res, next) => {
     let userId = parseInt(req.params.id)
@@ -23,3 +26,28 @@ const checkRole = async (req, res, next) => {
     }
 }
 module.exports = checkRole
+
+const checkRoleForPost = async (req, res, next) => {
+    let postId = parseInt(req.params.id)
+
+    if (!postId) {
+        return res.json(400).json({message: 'Missing data'})
+    }
+
+    let user = await User.findOne({where: {id: res.locals.id}, raw: true})
+    if (user) {
+        if (user.role !== 'admin') {
+            let post = await Post.findOne({where: {id: postId}, raw: true})
+
+            if (post.userId !== res.locals.id) {
+                return res.status(401).json({message: 'You are not owner of this account'})
+            }
+        }
+        res.locals = user.role;
+
+        next()
+    } else {
+        return res.status(401).json({message: 'Your account no longer exists'})
+    }
+}
+module.exports = checkRoleForPost
--- back/path/post.js
+++ back/path/post.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const checkToken = require('../middleware/jsonWebToken');
 const postCtrl = require('../controllers/post');
+const checkRoleForPost = require('../middleware/checkRole');
 let router = express.Router();
 
 router.put('', checkToken, postCtrl.createPost);
 router.get('/all', checkToken, postCtrl.getAllPosts);
 router.get('/:id', checkToken, postCtrl.getPost);
-router.patch('/:id', checkToken, postCtrl.updatePost);
+router.patch('/:id', checkToken, checkRoleForPost, postCtrl.updatePost);
 router.post('/:id/like', checkToken, postCtrl.likePost);
 router.post('/untrash/:id', checkToken, postCtrl.untrashPost);
 router.delete('/trash/:id', checkToken, postCtrl.trashPost);
