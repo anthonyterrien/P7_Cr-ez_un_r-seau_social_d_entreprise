2350db9c96edc706eda305b329f1ae5850f73255
anthony <anthony.terrien13@gmail.com>
2022-10-17 11:03:59 +0200

    -feat: signup

--- front/src/app/auth/signup/signup.component.ts
+++ front/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ICredential, ISignup } from '../../../models/user';
+import { AuthService } from '../../services/auth.service';
+import { TokenService } from '../../services/token.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,8 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() {}
+  form: ISignup = {
+    lastName: '',
+    firstName: '',
+    pseudo: '',
+    email: '',
+    password: ''
+  }
+  login: ICredential = {
+    pseudo: '',
+    password: ''
+  }
 
-  ngOnInit() {}
+  constructor(
+    private authService: AuthService,
+    private tokenService: TokenService
+  ) { }
 
+  ngOnInit(): void {
+  }
+
+  async onSubmit() {
+    this.authService.signup(this.form).subscribe(
+      data => {
+        this.login.pseudo = this.form.pseudo
+        this.login.password = this.form.password
+        this.authService.login(this.login).subscribe(
+          data => {
+            this.tokenService.saveToken(data.access_token)
+          },
+          err => console.log(err)
+        )
+      },
+      err => console.log(err)
+    )
+  }
 }
